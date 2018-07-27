7eebef4ebe6306187041ed149074221650534ba6
CodingApps <codingkit@gmail.com>
2018-07-26 20:18:07 -0700

    Initial Commit

---
+++ EBikeV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EBikeV1
+//
+//  Created by Rick Mc on 7/25/18.
+//  Copyright © 2018 Rick Mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
